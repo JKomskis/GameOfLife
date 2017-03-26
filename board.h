2767b0e7d7bdd5337098e71ec40cbd43baaf9602
Erika Bryant <erikabryant@Erikas-MacBook-Air.local>
2017-03-26 13:42:08 -0400

    updated board header file

--- board.h
+++ board.h
@@ -1,39 +1,34 @@
 //Header file for the board class
+#ifndef BOARD_H_
+#define BOARD_H_
+
 #include <string>
 #include <cstdlib>
-#include <iostream>
-#include <fstream>
+
 using namespace std;
 
 class Board{
+
 private:
+
 	bool ** matrix;
 	bool wrapAround;
 	int height;
 	int width;
 	//std::string fileName;	<--Do we need this in here????????
-	
-	
-	
+
 public:
+
 	Board(bool wrapAround, int height, int width);
+	Board(string filename);
 	void toggle(int &x, int &y);
 	void runIteration();
 	void runIteration(int runs);
 	void addPattern(string fileName, int x, int y);
 	bool ** getMatrix();
-	void saveState(string fileName, int height, int width);
-	
+	void saveState(string fileName);
+
 };
 
-/*Board::Board(bool wrapAround, int height, int width)
-{
-	bool this->wrapAround = wrapAround;
-	int this->height = height;
-	int this->width = width;	
-}
 
-Board(string fileName)
-{
-	std::string this -> fileName = fileName;
-} */
+#endif /* BOARD_H_ */
