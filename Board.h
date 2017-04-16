7e25b87fc308b12dd24a170de3f47e0cf21ce136
kjurczyk <kjurczyk@ufl.edu>
2017-04-16 14:00:15 -0400

    added some comments

--- Board.h
+++ Board.h
@@ -10,42 +10,39 @@
 
 class Board{
 
-protected:
+protected:	//protected variables
 
-	std::vector<std::vector<bool>> matrix;
-	int height;
-	int width;
+	std::vector<std::vector<bool>> matrix;	//boolean matrix that will store cells
+	int height;								//height of matrix
+	int width;								//width of matrix
+	bool wrapAround;						//allow the board to wrap around or not
+	int iterations;							//number of iterations that have been run
+	int births;								//number of births so far 
+	int deaths;								//number of deaths so far
+	bool isSaved;							//once the board has been modified, this is false
+	set<int> birthRule;						//sets the birthRule
+	set<int> survivalRule;					//sets the survivalRule
 
-	bool wrapAround;
+public:	//public functions and variables
 
-	int iterations;
-	int births;
-	int deaths;
-	bool isSaved;
-	set<int> birthRule;
-	set<int> survivalRule;
-
-public:
-
-	Board(bool wrapAround, int height, int width);
-	Board(std::string filename);
-	void toggle(int x, int y);
-	void randomize(double ratio);
-	void runIteration();
-	void runIteration(int runs);
-	void addPattern(std::string fileName, int x, int y);
-	void addPattern(std::vector<std::vector<bool>>, int x, int y);
-	void printBoard();
-	void saveState(std::string fileName);
-	std::vector<std::vector<bool>>& getMatrix();
-	int numNeigh(int r, int c);
-	int countNeigh(int r, int c);
-	int getHeight();
-	int getWidth();
-	int getIterations();
-	int getBirths();
-	int getDeaths();
-	bool getIsSaved();
+	Board(bool wrapAround, int height, int width);	//a constructor for the Board class if height, width, and wraparound options are chosen
+	Board(std::string filename);					//a constructor for the board class if just a filename is given
+	void toggle(int x, int y);						//toggles the cell from true to false or false to true
+	void randomize(double ratio);					//allows a board to be randomly generated
+	void runIteration();							//runs one iteration (for example, when the user presses the "Enter" key in the GameOfLife)
+	void runIteration(int runs);					//runs the interation the correct number of times
+	void addPattern(std::string fileName, int x, int y);	//allows the user to add an existing pattern to the board by calling with the filename, along with an x and y position
+	void addPattern(std::vector<std::vector<bool>>, int x, int y);	//allows the user to add an existing pattern to the board by calling with the actual bool matrix, along with an x and y position
+	void printBoard();								//prints the board as a matrix of 1s and 0s - good for testing purposes
+	void saveState(std::string fileName);			//save a given state or board, given a name for the file
+	std::vector<std::vector<bool>>& getMatrix();	//returns the matrix
+	int numNeigh(int r, int c);						//counts how many live neighbours a given cell has
+	int getHeight();								//returns the height of the board
+	int getWidth();									//returns the width of the board
+	int getIterations();							//returns the number of iterations that were run
+	int getBirths();								//returns the number of births that occurred
+	int getDeaths();								//returns the number of deaths that occurred
+	bool getIsSaved();								//returns a boolean value of if the board has been saved
 
 };
 
